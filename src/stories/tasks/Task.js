d7fc76711e2816d678882ab5081dc50b68b33894
Martin <martintsan@pm.me>
2021-05-27 14:56:12 +0800

    Updated

---
+++ src/stories/tasks/Task.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import PropTypes from "prop-types";
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+        ></span>
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
+      </div>
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== "TASK_ARCHIVED" && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`}></span>
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
+};
